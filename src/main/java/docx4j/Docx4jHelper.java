557f3e5707cbe1cbdf957f552e31cd1d2d074416
Luca Rautti <l.rautti@nexum.srl>
2020-05-13 14:37:10 +0200

    Adding methods for PDF

--- src/main/java/docx4j/Docx4jHelper.java
+++ src/main/java/docx4j/Docx4jHelper.java
@@ -16,10 +16,11 @@ import org.alfresco.model.ContentModel;
 import org.alfresco.repo.content.MimetypeMap;
 import org.alfresco.repo.content.transform.ContentTransformer;
 import org.alfresco.service.ServiceRegistry;
-import org.alfresco.service.cmr.model.FileExistsException;
+import org.alfresco.service.cmr.model.*;
 import org.alfresco.service.cmr.repository.ContentData;
 import org.alfresco.service.cmr.repository.ContentIOException;
 import org.alfresco.service.cmr.repository.ContentReader;
+import org.alfresco.service.cmr.repository.ContentService;
 import org.alfresco.service.cmr.repository.ContentWriter;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
@@ -44,9 +45,12 @@ public class Docx4jHelper {
 
 	
 	private ServiceRegistry serviceRegistry;
+	private FileFolderService fileFolderService;
+	private ContentService contentService;
 	private static Log logger = LogFactory.getLog(Docx4jHelper.class);
 	
 	org.docx4j.wml.ObjectFactory foo = Context.getWmlObjectFactory();
+	private static final long serialVersionUID = 1L;
 
 
 	public void setServiceRegistry(ServiceRegistry serviceRegistry) {
@@ -75,23 +79,30 @@ public class Docx4jHelper {
 	}
 	
 	
-	public void variableReplace(NodeRef nodeRef) {
+	public void variableReplace(NodeRef nodeRef) throws ContentIOException, Docx4JException {
 		
 		ContentReader reader = this.serviceRegistry.getContentService().getReader(nodeRef, ContentModel.PROP_CONTENT);
 		WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(reader.getContentInputStream());
-		MainDocumentPart documentPart = wordMLPackage.getMainDocumentPart();
-		org.docx4j.model.datastorage.migration.VariablePrepare.prepare(wordMLPackage);
-		HashMap<String, String> mappings = new HashMap<String, String>();
-		mappings.put("versioneAttuale", (this.getUltimaVersione(nodeRef)));
-		mappings.put("dataModifica", (this.getDataCorrente()));
-		documentPart.variableReplace(mappings);
-		if (save) {
-			SaveToZipFile saver = new SaveToZipFile(wordMLPackage);
-			saver.save(outputfilepath);
-		} else {
-			System.out.println(XmlUtils.marshaltoString(documentPart.getJaxbElement(), true,
-					true));
-		}
+		List<Object> texts = getAllElementFromObject(wordMLPackage.getMainDocumentPart(), Text.class);
+		String versioneAttuale = this.getUltimaVersione(nodeRef);
+		String dataModifica = this.getDataCorrente();
+		Docx4jHelper.searchAndReplace(texts, new HashMap<String, String>() {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 4561563843875288596L;
+
+			{
+				this.put("${versioneAttuale}", versioneAttuale);
+				this.put("${dataModifica}", dataModifica);
+			}
+
+			@Override
+            public String get(Object key) {
+                // TODO Auto-generated method stub
+                return super.get(key);
+            }
+		});   
 		
 	}
 	
@@ -113,24 +124,10 @@ public class Docx4jHelper {
 	}
 
 	
-	public String getDefaultName(NodeRef nodeRef) {
+	public String getDocumentName(NodeRef nodeRef) {
 		NodeService nodeService = serviceRegistry.getNodeService();
 		String nodeName = nodeService.getProperty(nodeRef, ContentModel.PROP_NAME).toString();
 		return nodeName;
-	}
-
-
-	
-	private Map<String, String> getMetadata(NodeRef nodeRef, Boolean includeBruckets) {
-		HashMap<String, String> mapping = new HashMap<String, String>();
-		NodeService nodeService = serviceRegistry.getNodeService();
-		Map<QName, Serializable> properties = nodeService.getProperties(nodeRef);
-		for (QName property : properties.keySet()) {
-			String variableName = includeBruckets ? "${" + property.getLocalName() + "}" : property.getLocalName();
-			String value = properties.get(property) == null ? "" : properties.get(property).toString();
-			mapping.put(variableName, value);
-		}
-		return mapping;
 	}
 
 	public NodeRef getTemplateInstance(NodeRef templateRef, NodeRef targetRef, String name) {
@@ -140,44 +137,49 @@ public class Docx4jHelper {
 		return instance;
 	}
 
-	/**
-	 * Substitute mapping variables into the docx4j document.
-	 * 
-	 * @param nodeRef.  NodeRef of the docx4j document
-	 * @param mappings. Map of name-value of all variables to substitute
-	 * @throws Docx4JException
-	 * @throws ContentIOException
+	
+	/*
+	 * * Questo metodo trasforma il documento Word docx in PDF
 	 */
-	@SuppressWarnings("deprecation")
-	public void printWordDocumentByMapping(NodeRef nodeRef, Map<String, String> mapping) throws ContentIOException, Docx4JException {
-		
-		ContentReader reader = this.serviceRegistry.getContentService().getReader(nodeRef, ContentModel.PROP_CONTENT);
-		WordprocessingMLPackage document = WordprocessingMLPackage.load(reader.getContentInputStream());
-		List<Object> texts = getAllElementFromObject(document.getMainDocumentPart(), Text.class);
-		texts.addAll(getAllElementFromObject(document.getHeaderFooterPolicy().getDefaultHeader(), Text.class));
-		replacePlaceholder(texts, mapping);
+	  
+	private NodeRef saveWordToPDF(NodeRef parentFolder,String fileName,WordprocessingMLPackage wordMLPackage) {
 
-		ContentWriter writer = this.serviceRegistry.getContentService().getWriter(nodeRef, ContentModel.PROP_CONTENT,
-				true);
-		document.save(writer.getContentOutputStream());
+		NodeRef ref = fileFolderService.create(parentFolder, fileName+".pdf", ContentModel.TYPE_CONTENT).getNodeRef();
+		ContentWriter cw = contentService.getWriter(ref, ContentModel.PROP_CONTENT, true);
+		cw.setMimetype(MimetypeMap.MIMETYPE_WORD);
+		try {
+			wordMLPackage.save(cw.getContentOutputStream());
+			return convertWordToPDF (ref);
+		}
+		catch (ContentIOException e) {e.printStackTrace();} 
+		catch (Docx4JException e) {e.printStackTrace();} 
+		return null;
+
 	}
 
-	/*
-	 
-	private void replacePlaceholder(List<Object> texts, Map<String, String> mapping) {
-		for (Object text : texts) {
-			Text textElement = (Text) text;
-			if (textElement.getValue().startsWith("${")) {
-				if (mapping.containsKey(textElement.getValue())) {
-					textElement.setValue(mapping.get(textElement.getValue()));
-				} else {
-					textElement.setValue("");
+	private NodeRef convertWordToPDF(NodeRef nodeRef) {
+
+		if (contentService.getReader(nodeRef, ContentModel.PROP_CONTENT) == null) return null;
+
+		try {
+			ContentReader cr = contentService.getReader(nodeRef, ContentModel.PROP_CONTENT);
+			ContentWriter cw = contentService.getWriter(nodeRef, ContentModel.PROP_CONTENT, true);
+			cw.setMimetype(MimetypeMap.MIMETYPE_PDF);
+			if (!cr.getMimetype().equals(MimetypeMap.MIMETYPE_PDF)){
+				ContentTransformer ct = contentService.getTransformer(cr.getMimetype(), MimetypeMap.MIMETYPE_PDF);
+				if(ct!=null){
+					ct.transform(cr, cw);
+					return nodeRef;
 				}
 			}
 		}
+		catch (FileExistsException e1) {e1.printStackTrace();}
+		catch(Exception e) {e.printStackTrace();}
+		return null;
+
 	}
-	*/
 	
+	/*
 	private void replacePlaceholder(WordprocessingMLPackage template,String name, String placeholder) {
 		
 		List<Object> texts = getAllElementFromObject(template.getMainDocumentPart(), Text.class);
@@ -188,7 +190,7 @@ public class Docx4jHelper {
             }
         }
     }
-	
+	*/
 	
 	/*
 	 * Questo metodo effettua il merge di più documenti Word
@@ -217,59 +219,125 @@ public class Docx4jHelper {
 		}
 		return docDest;
 	}
+*/
 	
-	
-	
-	
-	 
-	 * Questo metodo trasforma il documento Word docx in PDF
-	 * 
-	 
-	
-	private NodeRef saveWordToPDF(NodeRef parentFolder,String fileName,WordprocessingMLPackage wordMLPackage)
-	{
+	public static void searchAndReplace(List<Object> texts, Map<String, String> values){
 
-	NodeRef ref = fileFolderService.create(parentFolder, fileName+".pdf", ContentModel.TYPE_CONTENT).getNodeRef();
-	ContentWriter cw = contentService.getWriter(ref, ContentModel.PROP_CONTENT, true);
-	cw.setMimetype(MimetypeMap.MIMETYPE_WORD);
-	try {
+        // -- scan all expressions  
+        // Will later contain all the expressions used though not used at the moment
+        List<String> els = new ArrayList<String>(); 
 
-	wordMLPackage.save(cw.getContentOutputStream());
-	return convertWordToPDF (ref);
+        StringBuilder sb = new StringBuilder();
+        int PASS = 0;
+        int PREPARE = 1;
+        int READ = 2;
+        int mode = PASS;
 
-	} catch (ContentIOException e) {
+        // to nullify
+        List<int[]> toNullify = new ArrayList<int[]>();
+        int[] currentNullifyProps = new int[4];
 
-	e.printStackTrace();
+        // Do scan of els and immediately insert value
+        for(int i = 0; i<texts.size(); i++){
+            Object text = texts.get(i);
+            Text textElement = (Text) text;
+            String newVal = "";
+            String v = textElement.getValue();
+//          System.out.println("text: "+v);
+            StringBuilder textSofar = new StringBuilder();
+            int extra = 0;
+            char[] vchars = v.toCharArray();
+            for(int col = 0; col<vchars.length; col++){
+                char c = vchars[col];
+                textSofar.append(c);
+                switch(c){
+                case '$': {
+                    mode=PREPARE;
+                    sb.append(c);
+//                  extra = 0;
+                } break;
+                case '{': {
+                    if(mode==PREPARE){
+                        sb.append(c);
+                        mode=READ;
+                        currentNullifyProps[0]=i;
+                        currentNullifyProps[1]=col+extra-1;
+                        System.out.println("extra-- "+extra);
+                    } else {
+                        if(mode==READ){
+                            // consecutive opening curl found. just read it
+                            // but supposedly throw error
+                            sb = new StringBuilder();
+                            mode=PASS;
+                        }
+                    }
+                } break;
+                case '}': {
+                    if(mode==READ){
+                        mode=PASS;
+                        sb.append(c);
+                        els.add(sb.toString());
+                        newVal +=textSofar.toString()
+                                +(null==values.get(sb.toString())?sb.toString():values.get(sb.toString()));
+                        textSofar = new StringBuilder();
+                        currentNullifyProps[2]=i;
+                        currentNullifyProps[3]=col+extra;
+                        toNullify.add(currentNullifyProps);
+                        currentNullifyProps = new int[4];
+                        extra += sb.toString().length();
+                        sb = new StringBuilder();
+                    } else if(mode==PREPARE){
+                        mode = PASS;
+                        sb = new StringBuilder();
+                    }
+                }
+                default: {
+                    if(mode==READ) sb.append(c);
+                    else if(mode==PREPARE){
+                        mode=PASS;
+                        sb = new StringBuilder();
+                    }
+                }
+                }
+            }
+            newVal +=textSofar.toString();
+            textElement.setValue(newVal);
+        }
 
-	} catch (Docx4JException e) {
+        // remove original expressions
+        if(toNullify.size()>0)
+        for(int i = 0; i<texts.size(); i++){
+            if(toNullify.size()==0) break;
+            currentNullifyProps = toNullify.get(0);
+            Object text = texts.get(i);
+            Text textElement = (Text) text;
+            String v = textElement.getValue();
+            StringBuilder nvalSB = new StringBuilder();
+            char[] textChars = v.toCharArray();
+            for(int j = 0; j<textChars.length; j++){
+                char c = textChars[j];
+                if(null==currentNullifyProps) {
+                    nvalSB.append(c);
+                    continue;
+                }
+                // I know 100000 is too much!!! And so what???
+                int floor = currentNullifyProps[0]*100000+currentNullifyProps[1];
+                int ceil = currentNullifyProps[2]*100000+currentNullifyProps[3];
+                int head = i*100000+j;
+                if(!(head>=floor && head<=ceil)){
+                    nvalSB.append(c);
+                } 
 
-	e.printStackTrace();
-
-	} 
-	return null;
-
-	}
-
-	private NodeRef convertWordToPDF(NodeRef nodeRef) {
-
-		if (contentService.getReader(nodeRef, ContentModel.PROP_CONTENT) == null) return null;
-
-		try {
-			ContentReader cr = contentService.getReader(nodeRef, ContentModel.PROP_CONTENT);
-			ContentWriter cw = contentService.getWriter(nodeRef, ContentModel.PROP_CONTENT, true);
-			cw.setMimetype(MimetypeMap.MIMETYPE_PDF);
-			if (!cr.getMimetype().equals(MimetypeMap.MIMETYPE_PDF)){
-				ContentTransformer ct = contentService.getTransformer(cr.getMimetype(), MimetypeMap.MIMETYPE_PDF);
-				if(ct!=null){
-					ct.transform(cr, cw);
-					return nodeRef;
-				}
-			}
-		}
-		catch (FileExistsException e1)e1.printStackTrace();
-		catch(Exception e) e.printStackTrace();
-		return null;
-
-	}
-	*/
+                if(j>currentNullifyProps[3] && i>=currentNullifyProps[2]){
+                    toNullify.remove(0);
+                    if(toNullify.size()==0) {
+                        currentNullifyProps = null;
+                        continue;
+                    }
+                    currentNullifyProps = toNullify.get(0);
+                }
+            }
+            textElement.setValue(nvalSB.toString());
+        }
+    }
 }
